e44ac255c21145e00597a3e9d9411c19059bed19
Juliette Sinibardy <juju.siny@gmail.com>
2020-03-28 11:06:02 +0100

    :sparkles: Add a 1:M relationship between a User and Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_095738) do
+ActiveRecord::Schema.define(version: 2020_03_28_100036) do
 
   create_table "posts", force: :cascade do |t|
     t.string "url"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_095738) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

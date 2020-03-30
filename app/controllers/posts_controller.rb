3b8afeb6cccb2813c3c4a1858176fcd1be42a1df
Juliette Sinibardy <juju.siny@gmail.com>
2020-03-30 12:06:51 +0200

    :sparkles: Add New Post form and Alert box for errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,30 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(strong_params)
+    @post.creator = User.first # to-do: change once we add authentication
+
+    if @post.save
+      flash[:notice] = "Your post was created"
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def strong_params
+    params.require(:post).permit(:title, :url, :description)
   end
 end

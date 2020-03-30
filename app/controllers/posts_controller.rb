4e7f0ba851744a087c4c8a868d1008b93e482577
Juliette Sinibardy <juju.siny@gmail.com>
2020-03-30 12:21:53 +0200

    :recycle: Extract Post finding logic to before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -24,12 +25,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(strong_params)
       flash[:notice] = "The post has been updated"
       redirect_to post_path(@post)
@@ -42,5 +40,9 @@ class PostsController < ApplicationController
 
   def strong_params
     params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end

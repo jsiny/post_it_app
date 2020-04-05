2260eb4d4bfa2cdda132300cc825485fb720eea0
Juliette Sinibardy <juju.siny@gmail.com>
2020-04-05 12:39:41 +0200

    :ok_hand: Add generate_slug method to Category and User models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,11 @@ class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
 
+  before_save :generate_slug
+
   validates :name, presence: true
+
+  def generate_slug
+    self.slug = self.name.parameterize
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes
 
+  before_save :generate_slug
+
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: { minimum: 5 }
+
+  def generate_slug
+    self.slug = self.username.parameterize
+  end
 end

941b8dd0675794ad0869295f0a41fe44f34a8437
Juliette Sinibardy <juju.siny@gmail.com>
2020-04-06 14:02:10 +0200

    :recycle: Rewrite Voteable module with Rails Concern

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,27 +1,51 @@
+# Using Rails Concern
+
 module Voteable
-  def self.included(base)
-    base.send(:include, InstanceMethods)
-    base.extend ClassMethods
-    base.class_eval { establish_votes_relationship }
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :voteable
   end
 
-  module InstanceMethods
-    def total_votes
-      up_votes - down_votes
-    end
-
-    def up_votes
-      self.votes.where(vote: true).size
-    end
-  
-    def down_votes
-      self.votes.where(vote: false).size
-    end
+  def total_votes
+    up_votes - down_votes
   end
 
-  module ClassMethods
-    def establish_votes_relationship
-      has_many :votes, as: :voteable
-    end
+  def up_votes
+    self.votes.where(vote: true).size
+  end
+
+  def down_votes
+    self.votes.where(vote: false).size
   end
 end
+
+# Using normal Ruby metaprogramming
+
+# module Voteable
+#   def self.included(base)
+#     base.send(:include, InstanceMethods)
+#     base.extend ClassMethods
+#     base.class_eval { establish_votes_relationship }
+#   end
+
+#   module InstanceMethods
+#     def total_votes
+#       up_votes - down_votes
+#     end
+
+#     def up_votes
+#       self.votes.where(vote: true).size
+#     end
+  
+#     def down_votes
+#       self.votes.where(vote: false).size
+#     end
+#   end
+
+#   module ClassMethods
+#     def establish_votes_relationship
+#       has_many :votes, as: :voteable
+#     end
+#   end
+# end 
